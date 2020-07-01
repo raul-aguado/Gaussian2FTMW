ececef940910dbea6729f4feca8e97e0a7c5fd63
Raúl Aguado <56353268+raul-aguado@users.noreply.github.com>
2020-07-01 14:12:21 +0200

    Version 1.0
    Now, can prepare .COM files from .xyz files.
    Translated

--- Gaussian Output.py
+++ Gaussian Output.py
@@ -4,21 +4,51 @@ import sys
 import time
 
 print(""*100)
-print("        === Menu principal ===        ")
+print("           === Main menu ===           ")
 print()
-print("1. Comprobacion de archivos")
-print("2. Busqueda de constantes de rotacion")
+print("1. Prepare .COM files from .xyz files")
+print("2. Gaussian .log check")
+print("3. Rotational cosntants")
 print()
-print("======================================")
+print("=======================================")
 print()
-opt = input("Elija una opcion... ")
+opt = input("Choose an option... ")
 print(""*2)
 
 mylines = []
 chars = []
 
 if opt == "1":
-    ruta = input("Introduce la ruta de los acrhivos para analizar... ")
+    ruta = input("Path of .xyz files to prepare... ")
+    print()
+
+    filepaths = [arch.name for arch in scandir(ruta) if arch.is_file()]
+    for fp in filepaths:
+        ext = os.path.splitext(fp)[-1].lower()
+        
+        if ext == ".xyz":
+            ruta_completa = ruta + "/" + fp
+            com = ruta_completa.replace(".xyz", ".txt")
+            com_file = open(com, "w")
+
+            com_file.write("%mem=2048mb\n")
+            com_file.write("%nproc=2\n")
+            com_file.write("%chk=" + fp + "\n")
+            com_file.write("#B3LYP/6-311++G(d,p) opt freq output=pickett empiricaldispersion=GD3")
+            com_file.write(os.linesep)
+            com_file.write(fp )
+            com_file.write(os.linesep)
+            com_file.write("0 1\n")
+            
+            with open (ruta_completa, "r") as log:
+                for myline in log:
+                    com_file.write(myline)
+
+            com_file.write(os.linesep*10)
+            com_file.close()
+
+if opt == "2":
+    ruta = input("Path of files to check... ")
     print()
 
     filepaths = [arch.name for arch in scandir(ruta) if arch.is_file()]
@@ -39,15 +69,15 @@ if opt == "1":
                 linea_final = ''.join([str(elem) for elem in chars[0:31] if elem!=" "])
                 
                 if linea_final == "NormalterminationofGaussian":
-                    print("El archivo " + fp + " es correcto.")
+                    print("File " + fp + " is CORRECT.")
                     print()
 
                 else:
-                    print("El archivo " + fp + " no es correcto")
+                    print("Bad termination of " + fp + " file.")
                     print()
 
-elif opt == 2:
-    ruta = input("Archivo para analizar... ")
+elif opt == "3":
+    ruta = input("File to check... ")
     print()
 
     with open (ruta, "r") as log:
