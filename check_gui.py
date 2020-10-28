5cb0e03da2a943c7f7c0d0a7d52268ac516edd99
Raúl Aguado <56353268+raul-aguado@users.noreply.github.com>
2020-10-28 22:03:59 +0100

    Check GUI fix

--- check_gui.py
+++ check_gui.py
@@ -6,77 +6,87 @@ from tkinter import messagebox
 import time
 
 root = Tk()
+
 root.title("Gaussian2FTMW")
 
-def popup():
+def path_error_popup():
     messagebox.showerror("Error", "Path not found")
+
+def atoms_error_popup():
+    messagebox.showerror("Error", "Error in atom number")
 
 def search_click():
     path = path_entry.get()
-    n_atoms = atoms_entry.get()
+
+    try:
+        n_atoms = int(atoms_entry.get())
+    except:
+        atoms_error_popup()
 
     n_incorrect = 0
     n_correct = 0
 
     try:
         filepaths = [arch.name for arch in scandir(path) if arch.is_file()]
-        for fp in filepaths:
-            ext = os.path.splitext(fp)[-1].lower()
+    except:
+        path_error_popup()
         
-            if ext == ".log":
-                full_path = path + "/" + fp
+    for fp in filepaths:
+        ext = os.path.splitext(fp)[-1].lower()
+    
+        if ext == ".log":
+            full_path = path + "/" + fp
 
-                #First, the program checks that the last line in the log file is "NormalterminationofGaussian"
-                with open (full_path, "r") as log:
-                    for myline in log:
-                        mylines = []
-                        chars = []
+            #First, the program checks that the last line in the log file is "NormalterminationofGaussian"
+            with open (full_path, "r") as log:
+                mylines = []
+                chars = []
+                for myline in log:
+                    mylines.append(myline)
 
-                        mylines.append(myline)
+            n = int(((len(mylines))-1))
 
-                    n = (int(len(mylines))-1)
-
-                    for char in mylines[n]:
-                        chars.append(char)
-
-                    linea_final = ''.join([str(elem) for elem in chars[0:31] if elem!=" "])
                 
-                    if linea_final == "NormalterminationofGaussian":
-                        #If the last line in the log file is "NormalterminationofGaussian", the program checks that all the frequencies calculated are positives
-                        while mylines[n] != " and normal coordinates:\n":
-                            n=n-1
-                        else:
-                            n=n+3
-                            while mylines[n] != " - Thermochemistry -\n":
-                                chars = []
-                                for char in mylines[n]:
-                                    chars.append(char)
-                                freq_1 = ''.join([str(elem) for elem in chars[18:33] if elem!=" "])
-                                freq_2 = ''.join([str(elem) for elem in chars[34:60] if elem!=" "])
-                                freq_3 = ''.join([str(elem) for elem in chars[60:74] if elem!=" "])
-                    
-                                if (float(freq_1) < 0 or float(freq_2) < 0 or float(freq_3) < 0):
-                                    print("Error with the frequencies in " + full_path)
-                                    break
+            for char in mylines[n]:
+                chars.append(char)
 
+            linea_final = ''.join([str(elem) for elem in chars[0:31] if elem!=" "])
+
+            
+            if linea_final == "NormalterminationofGaussian":
+                #If the last line in the log file is "NormalterminationofGaussian", the program checks that all the frequencies calculated are positives
+                print(n)
+                while mylines[n] != " and normal coordinates:\n":
+                    n=n-1
+                else:
+                    n=n+3
+                    while mylines[n] != " - Thermochemistry -\n":
+                        chars = []
+                        for char in mylines[n]:
+                            chars.append(char)
+                        freq_1 = ''.join([str(elem) for elem in chars[18:33] if elem!=" "])
+                        freq_2 = ''.join([str(elem) for elem in chars[34:60] if elem!=" "])
+                        freq_3 = ''.join([str(elem) for elem in chars[60:74] if elem!=" "])
+            
+                        if (float(freq_1) < 0 or float(freq_2) < 0 or float(freq_3) < 0):
+                            print("Error with the frequencies in " + full_path)
+                            break
+                            
+                    
+                        else:
                             n = (n + n_atoms + 7)
                             
-                    else:
-                        print("Frequencies for " + fp + " are CORRECT")
+                    print("Frequencies for " + fp + " are CORRECT")
+                    n_correct = n_correct + 1
 
+            else:
+                n_incorrect = n_incorrect + 1 
+                incorrect_label = Label(root, text = ("Bad termination of " + fp + " file."))
+                incorrect_label.grid(row = 5, column = 0, columnspan = 2)
 
-                        n_correct = n_correct + 1
+    result_label = Label(root, text = (str(n_correct) + " files are CORRECT and " + str(n_incorrect) + " files have a BAD termination"))
+    result_label.grid(row = 6, column = 0, columnspan = 2)
 
-                    else:
-                        n_incorrect = n_incorrect + 1 
-                        incorrect_label = Label(root, text = ("Bad termination of " + fp + " file."))
-                        incorrect_label.grid(row = 5, column = 0, columnspan = 2)
-
-        result_label = Label(root, text = (str(n_correct) + " files are CORRECT and " + str(n_incorrect) + " files have a BAD termination"))
-        result_label.grid(row = 6, column = 0, columnspan = 2)
-
-    except:
-        popup()
 
 
 def exit_click():
